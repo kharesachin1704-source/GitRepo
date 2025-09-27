dd8208f62af8f671a5b68fdc0206152bd8d3ff36
root <root@ip-172-31-25-172.ec2.internal>
2025-09-27 18:39:59 +0000

    initial commit

---
+++ src/main/java/com/sajal/ec2demo/App.java
@@ -0,0 +1,13 @@
+package com.sajal.ec2demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
